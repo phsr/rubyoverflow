f85c6912276be77d2f7330d1d920b72be304ef73
phsr <git@danseaver.com>
2011-07-27 20:53:44 -0400

    DRYing up user_spec

--- spec/users_spec.rb
+++ spec/users_spec.rb
@@ -1,23 +1,23 @@
 require File.dirname(__FILE__) + "/spec_helper"
 
 describe Users do
+	before(:each) do
+		@client = Client.new
+	end
   it "retrieves sites" do
-		client = Client.new
-		hash, url = client.users.fetch
+		hash, url = @client.users.fetch
 		url.should == 'http://api.stackoverflow.com/1.1/users/'
 		hash['users'].should be_instance_of Array
   end
 
 	it "retrieves phsr" do
-		client = Client.new
-		hash, url = client.users.fetch(:id => 53587)
+		hash, url = @client.users.fetch(:id => 53587)
 		url.should == 'http://api.stackoverflow.com/1.1/users/53587/'
 		hash['users'].first['display_name'].should == 'phsr'
 	end
 
 	it "retrieves multiple users by an array of ids" do
-		client = Client.new
-		hash, url = client.users.fetch(:id => [53587,22656])
+		hash, url = @client.users.fetch(:id => [53587,22656])
 		url.should == 'http://api.stackoverflow.com/1.1/users/53587;22656/'
 		hash['users'].count.should == 2
 	end
